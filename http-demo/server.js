991dd79075f9b9e1741fd7dbfa4acb6f6f8b4032
xyyojl <m13631897256@163.com>
2020-03-05 16:52:53 +0800

    理解CORS跨域请求的限制与解决

--- http-demo/server.js
+++ http-demo/server.js
@@ -1,9 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function(request, response){
     console.log('request name',request.url);
 
-    response.end('123');
+    const html = fs.readFileSync('test.html','utf8');
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    response.end(html);
 }).listen(8888);
 
 console.log('server listening on 8888');
---
+++ http-demo/server2.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+http.createServer(function(request, response){
+    console.log('request name',request.url);
+
+    response.writeHead(200, {
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:8888'
+    })
+    response.end('123');
+}).listen(8887);
+
+console.log('server listening on 8887');
