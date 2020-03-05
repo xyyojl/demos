996a86011c5d2287dd982e329876c0b768b4d07a
xyyojl <m13631897256@163.com>
2020-03-05 17:27:28 +0800

    学习CORS跨域限制以及预请求验证

--- http-demo/server2.js
+++ http-demo/server2.js
@@ -4,7 +4,10 @@ http.createServer(function(request, response){
     console.log('request name',request.url);
 
     response.writeHead(200, {
-        'Access-Control-Allow-Origin': 'http://127.0.0.1:8888'
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:8888',
+        'Access-Control-Allow-Headers': 'X-Test-Cors',
+        'Access-Control-Allow-Methods': 'POST,PUT,Delete',
+        'Access-Control-Max-Age': '1000',
     })
     response.end('123');
 }).listen(8887);
