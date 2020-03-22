70f4282ae7a376c91b7fd707446ae6971e22e0c8
xyyojl <m13631897256@163.com>
2020-03-22 15:27:39 +0800

    理解 服务端代理

--- crossOrigin-demo/serverProxy/server.js
+++ crossOrigin-demo/serverProxy/server.js
@@ -4,6 +4,7 @@ const url = require('url');
 const http = require('http');
 const https = require('https');
 
+// 在浏览器访问：http://127.0.0.1:3000/topics
 const server = http.createServer((req,res) => {
 	const path = url.parse(req.url).path.slice(1);
 	if(path === 'topics'){
@@ -23,27 +24,3 @@ const server = http.createServer((req,res) => {
 }).listen(3000, '127.0.0.1');
 
 console.log('启动服务，监听 127.0.0.1:3000')
-
-
-/* const url = require('url');
-const http = require('http');
-const https = require('https');
-
-const server = http.createServer((req, res) => {
-	const path = url.parse(req.url).path.slice(1);
-	if(path === 'topics') {
-		https.get('https://cnodejs.org/api/v1/topics', (resp) => {
-			let data = "";
-			resp.on('data', chunk => {
-				data += chunk;
-			});
-			resp.on('end', () => {
-				res.writeHead(200, {
-					'Content-Type': 'application/json; charset=utf-8'
-				});
-				res.end(data);
-			});
-		})		
-	}
-}).listen(3000, '127.0.0.1');
-console.log('启动服务，监听 127.0.0.1:3000'); */
