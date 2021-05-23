91096bf713dbc6ae42510a2220e1e59f6c7a6849
xyyojl <m13631897256@163.com>
2021-05-23 17:34:09 +0800

    特性检测，兼容移动端

--- jirengu/canvas-demo/main.js
+++ jirengu/canvas-demo/main.js
@@ -15,14 +15,15 @@
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-var using = false;
 var lineWidth = 5;
 
 // 1. 自动设置 canvas 的尺寸
 autoCanvasSize(canvas);
 
+// 2. 监听用户操作
+listenToUser();
 
-// 橡皮擦
+// 功能：橡皮擦、画笔、垃圾桶、下载
 var eraserEnabled = false;
 eraser.onclick = function () {
     eraserEnabled = true;
@@ -40,6 +41,7 @@ clear.onclick = function () {
 download.onclick = function () {
     var url = canvas.toDataURL("image/png");
     var a = document.createElement('a');
+    document.body.appendChild(a);
     a.href = url;
     a.download = '我的画画';
     a.target = '_blank';
@@ -77,39 +79,71 @@ blue.onclick = function () {
 }
 
 // 选择画笔粗细
-thin.onclick = function() {
+thin.onclick = function () {
     lineWidth = 5;
 }
-thick.onclick = function() {
+thick.onclick = function () {
     lineWidth = 10;
 }
 
-//上一次的那个点
-var lastPoint = { "x": undefined, "y": undefined };
-
-canvas.onmousedown = function (e) {
-    using = true;
-    var x = e.clientX;
-    var y = e.clientY;
-    lastPoint = { "x": x, "y": y };
-    // 画圆不是必须的，辅助理解而已
-    // drawCircle(x, y, 2.5);
-}
-canvas.onmousemove = function (e) {
-    if (!using) return;
-    var x = e.clientX;
-    var y = e.clientY;
-    if (eraserEnabled) {
-        context.clearRect(x - 5, y - 5, 10, 10)
+function listenToUser(params) {
+    var using = false;
+    //上一次的那个点
+    var lastPoint = { "x": undefined, "y": undefined };
+    // 特性检测
+    if (document.body.ontouchstart !== undefined) {
+        // 触屏设备，因为 document.body.ontouchstart 的值为 null
+        canvas.ontouchstart = function (e) {
+            using = true;
+            var x = e.touches[0].clientX;
+            var y = e.touches[0].clientY;
+            lastPoint = { "x": x, "y": y };
+            // 画圆不是必须的，辅助理解而已
+            // drawCircle(x, y, 2.5);
+        }
+        canvas.ontouchmove = function (e) {
+            if (!using) return;
+            var x = e.touches[0].clientX;
+            var y = e.touches[0].clientY;
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = { "x": x, "y": y };
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                // 更新上一次的那个点
+                lastPoint = newPoint;
+            }
+        }
+        canvas.ontouchend = function () {
+            using = false;
+        }
     } else {
-        var newPoint = { "x": x, "y": y };
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-        // 更新上一次的那个点
-        lastPoint = newPoint;
+        // 非触屏设备
+        canvas.onmousedown = function (e) {
+            using = true;
+            var x = e.clientX;
+            var y = e.clientY;
+            lastPoint = { "x": x, "y": y };
+            // 画圆不是必须的，辅助理解而已
+            // drawCircle(x, y, 2.5);
+        }
+        canvas.onmousemove = function (e) {
+            if (!using) return;
+            var x = e.clientX;
+            var y = e.clientY;
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = { "x": x, "y": y };
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                // 更新上一次的那个点
+                lastPoint = newPoint;
+            }
+        }
+        canvas.onmouseup = function () {
+            using = false;
+        }
     }
-}
-canvas.onmouseup = function () {
-    using = false;
 }
 
 
@@ -124,11 +158,11 @@ function drawCircle(x, y, radius) {
 // 画线
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
-    // context.strokeStyle = '#000';
     context.lineWidth = lineWidth;
     context.moveTo(x1, y1); // 起点
     context.lineTo(x2, y2); // 终点
     context.stroke();
+    context.closePath();
 }
 
 // 设置canvas的宽高
