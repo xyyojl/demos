8460013e4a36b9deed493ff110258f9c5e0e4975
xyyojl <m13631897256@163.com>
2021-05-29 18:04:44 +0800

    处理post请求

--- imooc/node-study/http-test/app.js
+++ imooc/node-study/http-test/app.js
@@ -1,4 +1,28 @@
+// post 请求
 const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        // 数据格式
+        console.log('content-type', req.headers['content-type']);
+        // 接收数据
+        let postData = "";
+        req.on('data', chunk => {
+            postData += chunk.toString();
+        })
+        req.on('end', () => {
+            console.log('postData', postData);
+            res.end('hello world') // 在这里返回，因为是异步
+        })
+
+    }
+})
+
+server.listen(8000);
+console.log('OK');
+
+// get 请求
+/* const http = require('http');
 const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
@@ -13,4 +37,4 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(8000);
-console.log('OK');
+console.log('OK'); */
