72be9b645d538ad7830d33ce57a3b2147e2b651b
xyyojl <m13631897256@163.com>
2021-06-12 16:06:44 +0800

    学习优化代码思路

--- jirengu/jquery-api/2.js
+++ jirengu/jquery-api/2.js
@@ -17,11 +17,18 @@ function addClass(node) {
     var classes = { 'a': true, 'b': false, 'c': true };
     for (let key in classes) {
         var value = classes[key];
-        if (value) {
+        // 优化代码：如果存在类似的代码，就有优化的可能
+        // 优化前的代码
+        /* if (value) {
             node.classList.add(key);
         } else {
             node.classList.remove(key);
-        }
+        } */
+
+        // 根据 value，得到对应的方法名
+        var methodName = value ? 'add': 'remove';
+        node.classList[methodName](key);
+        
     }
 }
 
