89a23ba12629cd1bf8621f20cbf5661a7b10988e
xyyojl <m13631897256@163.com>
2021-06-13 16:01:50 +0800

    第二版：支持自动播放，鼠标移动上去，暂停轮播；鼠标离开，开启轮播

--- jirengu/slides-demo/2.js
+++ jirengu/slides-demo/2.js
@@ -8,5 +8,41 @@ for (let i = 0; i < allButtons.length; i++) {
         $('#images').css({
             transform: 'translateX(' + distance + 'px)'
         })
+        // 给当前选中的按钮，加上class，给 class 的 red 的元素，移除 class
+        allButtons.eq(index)
+            .addClass('red')
+            .siblings('.red').removeClass('red');
     })
 }
+
+
+// 自动播放
+let n = 0;
+let size = allButtons.length;
+allButtons.eq(n % size).trigger('click')
+    // 添加 class
+    .addClass('red')
+    // 移除 class
+    .siblings('.red').removeClass('red');
+
+let timerId = setInterval(() => {
+    n += 1;
+    allButtons.eq(n % size).trigger('click')
+        .addClass('red')
+        .siblings('.red').removeClass('red');
+}, 3000);
+
+$('.window').on('mouseenter', function() {
+    window.clearInterval(timerId);
+})
+
+$('.window').on('mouseleave', function() {
+    // 开启闹钟
+    timerId = setInterval(() => {
+        n += 1;
+        allButtons.eq(n % size).trigger('click')
+            .addClass('red')
+            .siblings('.red').removeClass('red');
+    }, 3000);
+    
+})
