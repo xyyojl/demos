6e027a8235205f1fbaa5eb7ec88cd8a864a7f67e
xyyojl <m13631897256@163.com>
2021-06-13 12:44:34 +0800

    第一版：实现点击按钮，切换到不同的图片

---
+++ jirengu/slides-demo/1.js
@@ -0,0 +1,20 @@
+$('#p1').on('click', function() {
+    $('#images').css({
+        transform: 'translateX(0)'
+    })
+})
+$('#p2').on('click', function() {
+    $('#images').css({
+        transform: 'translateX(-300px)'
+    })
+})
+$('#p3').on('click', function() {
+    $('#images').css({
+        transform: 'translateX(-600px)'
+    })
+})
+$('#p4').on('click', function() {
+    $('#images').css({
+        transform: 'translateX(-900px)'
+    })
+})
