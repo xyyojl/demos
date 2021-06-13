65c0a8025c4a3515a1f43e2b15d42e50220217fe
xyyojl <m13631897256@163.com>
2021-06-13 16:18:26 +0800

    第三版：优化重复代码，封装成函数

--- jirengu/slides-demo/2.js
+++ jirengu/slides-demo/2.js
@@ -3,15 +3,13 @@ let allButtons = $('#buttons span');
 for (let i = 0; i < allButtons.length; i++) {
     // 给每个 span 绑定 click 事件
     $(allButtons[i]).on('click', function (e) {
+        // 找到当前元素的索引
         let index = $(e.currentTarget).index();
         let distance = index * -300;
         $('#images').css({
             transform: 'translateX(' + distance + 'px)'
         })
-        // 给当前选中的按钮，加上class，给 class 的 red 的元素，移除 class
-        allButtons.eq(index)
-            .addClass('red')
-            .siblings('.red').removeClass('red');
+        activeButton(allButtons.eq(index));
     })
 }
 
@@ -20,29 +18,31 @@ for (let i = 0; i < allButtons.length; i++) {
 let n = 0;
 let size = allButtons.length;
 allButtons.eq(n % size).trigger('click')
-    // 添加 class
-    .addClass('red')
-    // 移除 class
-    .siblings('.red').removeClass('red');
 
-let timerId = setInterval(() => {
-    n += 1;
-    allButtons.eq(n % size).trigger('click')
-        .addClass('red')
-        .siblings('.red').removeClass('red');
-}, 3000);
+let timerId = setTimer();
+
 
 $('.window').on('mouseenter', function() {
+    // 砸掉闹钟
     window.clearInterval(timerId);
 })
 
 $('.window').on('mouseleave', function() {
     // 开启闹钟
-    timerId = setInterval(() => {
-        n += 1;
-        allButtons.eq(n % size).trigger('click')
-            .addClass('red')
-            .siblings('.red').removeClass('red');
-    }, 3000);
-    
+    timerId = setTimer();
 })
+
+// 定时器 - 闹钟
+function setTimer() {
+    return setInterval(() => {
+        n += 1;
+        allButtons.eq(n % size).trigger('click');
+    }, 3000);
+}
+// 激活按钮
+function activeButton($button) {
+    // 给当前选中的按钮，加上class，给 class 的 red 的元素，移除 class
+    $button
+        .addClass('red')
+        .siblings('.red').removeClass('red');
+}
