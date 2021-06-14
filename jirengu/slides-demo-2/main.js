2701d009509f927b8946c1e9b711a788baac787d
xyyojl <m13631897256@163.com>
2021-06-14 16:52:00 +0800

    用状态机来理解轮播，三个类代表三个状态

--- jirengu/slides-demo-2/main.js
+++ jirengu/slides-demo-2/main.js
@@ -1,49 +1,37 @@
+$('.images > img:nth-child(1)').addClass('current');
+$('.images > img:nth-child(2)').addClass('enter');
 setTimeout(() => {
-    $('.images > img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    });
-    $('.images > img:nth-child(2)').css({ 
-        transform: 'translateX(-100%)'
-    });
-    $('.images > img:nth-child(1)').one('transitionend', function(e) {
-        $(e.currentTarget).addClass('right').css({ transform: 'none' });
-    })
+    // 给第一个移除 current 类，加上 leave 类
+    // 动画结束的时候，移除 leave 类，加上 enter 类
+    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter');
+        })
+
+    // 给第二个移除 enter 类，加上 current 类
+    $('.images > img:nth-child(2)').removeClass('enter').addClass('current');
 }, 3000);
 
 setTimeout(() => {
-    $('.images > img:nth-child(2)').css({
-        transform: 'translateX(-200%)'
-    });
-    $('.images > img:nth-child(3)').css({ 
-        transform: 'translateX(-100%)'
-    });
-    $('.images > img:nth-child(2)').one('transitionend', function(e) {
-        $(e.currentTarget).addClass('right').css({ transform: 'none' });
-    })
+    $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter');
+        })
+    $('.images > img:nth-child(3)').removeClass('enter').addClass('current');
 }, 6000);
 
-
 setTimeout(() => {
-    $('.images > img:nth-child(3)').css({
-        transform: 'translateX(-200%)'
-    });
-    $('.images > img:nth-child(1)').css({ 
-        transform: 'translateX(-100%)'
-    });
-    $('.images > img:nth-child(3)').one('transitionend', function(e) {
-        $(e.currentTarget).addClass('right').css({ transform: 'none' });
-    })
+    $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter');
+        })
+    $('.images > img:nth-child(1)').removeClass('enter').addClass('current');
 }, 9000);
 
 setTimeout(() => {
-    $('.images > img:nth-child(1)').css({
-        transform: 'translateX(-200%)'
-    });
-    $('.images > img:nth-child(2)').css({ 
-        transform: 'translateX(-100%)'
-    });
-    $('.images > img:nth-child(1)').one('transitionend', function(e) {
-        $(e.currentTarget).addClass('right').css({ transform: 'none' });
-    })
+    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter');
+        })
+    $('.images > img:nth-child(2)').removeClass('enter').addClass('current');
 }, 12000);
-
