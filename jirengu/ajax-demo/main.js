886c63dcb9a745b39c271c82dd4b36294c10a58c
xyyojl <m13631897256@163.com>
2021-06-28 21:16:30 +0800

    删除一些没用的代码

--- jirengu/ajax-demo/main.js
+++ jirengu/ajax-demo/main.js
@@ -6,21 +6,13 @@ myButton.addEventListener('click', (e) => {
     // request.open('get', 'http://localhost:8002/xxx'); // 配置 request
     request.send();
     request.onreadystatechange = function() {
-        console.log(request.readyState);
         if(request.readyState === 4) {
-            console.log('请求响应都完毕了');
-            console.log('request.status', request.status);
             if(request.status >= 200 & request.status < 300) {
-                console.log('说明请求成功了');
-                console.log(typeof request.responseText);
-                console.log('request.responseText', request.responseText);
                 let string = request.responseText;
                 // 把符合 JSON 语法的字符串转换成 JS 对应的值
                 let object = window.JSON.parse(string);
                 // JSON.parse 是浏览器提供的
-                console.log(typeof object);
-                console.log('object', object);
-                console.log('object.note', object.note);
+                console.log(object);
             } else if(request.status >= 400) {
                 console.log('说明请求失败');
             }
