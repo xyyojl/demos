df0f87b4f14fc4b58caf3cdf401b864ef5b439a5
xyyojl <m13631897256@163.com>
2021-07-15 20:21:42 +0800

    使message.js模块化，添加message.js的M

--- jirengu/resume/js/message.js
+++ jirengu/resume/js/message.js
@@ -1,56 +1,89 @@
-var APP_ID = 'QQ1Nqrp88ugKFRtId9KknVKl-gzGzoHsz';
-var APP_KEY = 'Jgi6JnSQIp2aixTwjHEi12o1';
+!(function () {
+    var view = document.querySelector('section.message');
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-})
+    var model = {
+        init() {
+            var APP_ID = 'QQ1Nqrp88ugKFRtId9KknVKl-gzGzoHsz';
+            var APP_KEY = 'Jgi6JnSQIp2aixTwjHEi12o1';
 
-// 获取数据
-const query = new AV.Query('Message');
-query.find().then(function (messages) {
-    // messages 是一个数组
-    const array = messages.map(item => item.attributes);
-    // 将数据插入到页面
-    array.forEach(item => {
-        const li = document.createElement('li');
-        li.innerText = `${item.name}: ${item.content}`;
-        const messageList = document.querySelector('#messageList');
-        messageList.appendChild(li);
-    });
-    // 下面两个东西都没有用
-}).then(function() {
-    // 更新成功
-}, function(error) {
-    // 异常处理
-})
+            AV.init({
+                appId: APP_ID,
+                appKey: APP_KEY
+            })
 
-// 提交数据
-let myForm = document.querySelector('#postMessageForm');
-myForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    // 获取到 content 和 name，调用方法，进行提交
-    const name = myForm.querySelector('input[name=name]').value;
-    const content = myForm.querySelector('textarea[name=content]').value;
-    if(name === '' || content === '') {
-        alert('想要留下的信息，请记得输入姓名和留言内容，谢谢');
-        return;
-    }
-    const Message = AV.Object.extend('Message');
-    const message = new Message();
-    message.save({
-        'name': name,
-        'content': content,
-    }).then(function (object) {
-        console.log('success');
-        // 提交成功，不刷新页面，插入一条数据即可
-        // attributes
-        const { name, content } = object.attributes;
-        const li = document.createElement('li');
-        li.innerText = `${name}: ${content}`;
-        const messageList = document.querySelector('#messageList');
-        messageList.appendChild(li);
-        myForm.querySelector('input[name=name]').value = '';
-        myForm.querySelector('textarea[name=content]').value = '';
-    })
-})
+        },
+        // 获取数据
+        fetch() {
+            const query = new AV.Query('Message');
+            return query.find(); // Promise 对象
+        },
+        // 保存数据
+        save(name, content) {
+            const Message = AV.Object.extend('Message');
+            const message = new Message();
+            return message.save({ // Promise 对象
+                'name': name,
+                'content': content,
+            })
+        }
+    };
+
+    var controller = {
+        view: null,
+        model: null,
+        messageList: null,
+        form: null,
+        init(view, model) {
+            this.view = view;
+            this.model = model;
+            this.messageList = view.querySelector('#messageList');
+            this.form = view.querySelector('form');
+
+            this.model.init();
+            this.loadMessages();
+            this.bindEvents();
+
+        },
+        bindEvents() {
+            this.form.addEventListener('submit', function (e) {
+                e.preventDefault();
+                this.saveMessages();
+            }.bind(this));
+        },
+        loadMessages() {
+            model.fetch().then(function (messages) {
+                // messages 是一个数组
+                const array = messages.map(item => item.attributes);
+                // 将数据插入到页面
+                array.forEach(item => {
+                    const li = document.createElement('li');
+                    li.innerText = `${item.name}: ${item.content}`;
+                    this.messageList.appendChild(li);
+                });
+            })
+        },
+        saveMessages() {
+            // 获取到 content 和 name，调用方法，进行提交
+            // this.form 重复太多，之前是手动修改的，可以给 myForm 赋值为 this.form
+            const myForm = this.form;
+            const name = myForm.querySelector('input[name=name]').value;
+            const content = myForm.querySelector('textarea[name=content]').value;
+            if (name === '' || content === '') {
+                alert('想要留下的信息，请记得输入姓名和留言内容，谢谢');
+                return;
+            }
+            model.save(name, content).then(function (object) {
+                console.log('success');
+                // 提交成功，不刷新页面，插入一条数据即可
+                const { name, content } = object.attributes;
+                const li = document.createElement('li');
+                li.innerText = `${name}: ${content}`;
+                this.messageList.appendChild(li);
+                myForm.querySelector('input[name=name]').value = '';
+                myForm.querySelector('textarea[name=content]').value = '';
+            })
+        }
+    };
+
+    controller.init(view, model);
+}).call();
